9a0c9056648deb71452cc3b31a48e947169b78a3
Batur166 <baturturan2@gmail.com>
2025-09-26 00:57:01 +0400

    'imnubygvfcdexsw'

--- routes/notificationroute.js
+++ routes/notificationroute.js
@@ -5,7 +5,7 @@ const authenticateuser=require('../middleware/adminmiddleware');
 const adminmiddleware = require("../middleware/adminmiddleware");
 
 // Add authentication middleware to notification routes
-router.post("/sendnotification", adminmiddleware, notifconroller.sendcumanotification);
+router.post("/sendnotification", notifconroller.sendcumanotification);
 
 module.exports = router
 
