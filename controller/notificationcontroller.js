650dd990a67c85fc48cdd7f71f48d3a1a69dc11f
Batur166 <baturturan2@gmail.com>
2025-09-29 00:24:05 +0400

    'wefwrgtwnrhgeabfsVD'

--- controller/notificationcontroller.js
+++ controller/notificationcontroller.js
@@ -71,7 +71,7 @@ exports.sendcumanotification = async (req, res) => {
         mescidId,
         sentTo: users.map((u) => u._id),
         sentCount: sendResult.successCount || users.length,
-        status: sendResult && sendResult.failureCount > 0 ? "pending" : "read",
+        status:"pending",
         data: {
           screen: "MainPage",
           params: { mescidId, senderName: sender.name },
